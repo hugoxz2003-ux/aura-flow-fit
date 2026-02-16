22fd8948ab5610fb0b8f4cc18ecf42052bfd94c2
hugoxz2003-ux <hugoxz2003@gmail.com>
2026-02-16 18:13:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             try {
                 // Send to n8n Webhook
-                await fetch('https://auraflowfit.app.n8n.cloud/webhook/6539874a-c7e6-4e03-a964-0a5de374fdf5', {
+                await fetch('https://auraflowfit.app.n8n.cloud/webhook-test/6539874a-c7e6-4e03-a964-0a5de374fdf5', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
